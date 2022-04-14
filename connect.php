e8061f33397c45ab3e1a4987a8dfaabc1e856aef
Liao enchang <h86623547@gmail.com>
2022-04-14 08:41:13 +0800

    a

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-   $host        = "host=ec2-3-218-171-44.compute-1.amazonaws.com";
+   $host        = "host=ec2-54-173-77-184.compute-1.amazonaws.com";
    $port        = "port=5432";
-   $dbname      = "dbname=d7fj66inqi0dpm";
-   $credentials = "user=rzsczkwppbtphe password=c8a0aa6b0ed6e793bcaee8291396c596bce8b7fde75e8e0235001b14702528f7";
+   $dbname      = "dbname=dfl03sou1d8gk9";
+   $credentials = "user=lgzvimsouelcqj password=c3508f62d574dec30263e723ea42311f77af9c6823db7be85c0c874ac041735d";
 
    $db = pg_connect( "$host $port $dbname $credentials"  );
 ?>
