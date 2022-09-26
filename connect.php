4fdbd717b47d893229f4013555e388ac6ab234ac
Jeff671 <114133466+Jeff671@users.noreply.github.com>
2022-09-26 17:22:11 +0800

    program

--- connect.php
+++ connect.php
@@ -1,8 +1,9 @@
 <?php
-   $host        = "host=ec2-54-173-77-184.compute-1.amazonaws.com";
-   $port        = "port=5432";
-   $dbname      = "dbname=dfl03sou1d8gk9";
-   $credentials = "user=lgzvimsouelcqj password=c3508f62d574dec30263e723ea42311f77af9c6823db7be85c0c874ac041735d";
+	#Enter ur OWN host,port,datebase name and user name
+   $host        = "host=";
+   $port        = "port=";
+   $dbname      = "dbname=";
+   $credentials = "user=";
 
    $db = pg_connect( "$host $port $dbname $credentials"  );
 ?>
